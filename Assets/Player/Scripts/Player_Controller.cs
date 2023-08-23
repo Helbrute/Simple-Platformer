fa33f45e6f41e4f467e3beb62c04aabc4fdcb910
helbture <bartosz.drabik4@gmail.com>
2023-08-23 10:14:41 +0200

    Character movement
    Added player movement function

--- Assets/Player/Scripts/Player_Controller.cs
+++ Assets/Player/Scripts/Player_Controller.cs
@@ -7,20 +7,21 @@ public class Player_Controller : MonoBehaviour
 {
     #region Player Components
     Rigidbody2D rb;
+    Animator anim;
     CapsuleCollider2D capsCollider;
-    Vector2 moveInput;
     #endregion
 
     #region Player Variables
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float jumpPower = 10f;
     [SerializeField] float baseGravity = 5f;
+    [SerializeField] float horizontal;
     #endregion
 
     #region Ground Check
     [SerializeField] float groundCheckDistance = 0.5f;
-    //[SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundLayer;
+    RaycastHit2D[] groundCheckHit = new RaycastHit2D[5];
+    ContactFilter2D groundCheckFilter;
     [SerializeField] bool groundCheck;
     public bool isGrounded
     {
@@ -31,6 +32,7 @@ public class Player_Controller : MonoBehaviour
         private set
         { 
             groundCheck = value;
+            anim.SetBool("isJumping", !value);
         }
     }
     #endregion
@@ -38,16 +40,65 @@ public class Player_Controller : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         capsCollider = GetComponent<CapsuleCollider2D>();
     }
 
+
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        //make player move
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        
+
+        //Methods
         GroundCheck();
+        FlipSrpite();
+        //anim.SetBool("isJumping", !isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        
+        if (isGrounded && context.performed)
+        {
+            //anim.SetBool("isJumping", true);
+            rb.velocity = new Vector2(rb.velocity.y, jumpPower);
+        }
+        else if (context.canceled && rb.velocity.y != 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().normalized.x;
+
+        //Running Animation
+        if (horizontal != 0)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+
+    void FlipSrpite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 
     void GroundCheck()
     {
-        groundCheck = Physics2D.OverlapCircle(transform.position, groundCheckDistance, groundLayer);
+        isGrounded = capsCollider.Cast(Vector2.down, groundCheckFilter, groundCheckHit, groundCheckDistance) > 0;
     }
 }
