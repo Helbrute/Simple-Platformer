57a43031c363f519957a683562e518670dbd2336
helbture <bartosz.drabik4@gmail.com>
2023-08-24 09:46:11 +0200

    Implemented Ladder mechanic
    Implemented OnLadder function using new Input System and vertical input asset,  now player can climb the ladder. To the sky!

--- Assets/Player/Scripts/Player_Controller.cs
+++ Assets/Player/Scripts/Player_Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,13 +10,17 @@ public class Player_Controller : MonoBehaviour
     Rigidbody2D rb;
     Animator anim;
     CapsuleCollider2D capsCollider;
+    Sprite ladderStopSprite;
     #endregion
 
     #region Player Variables
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float jumpPower = 10f;
+    [SerializeField] float climbSpeed = 5f;
     [SerializeField] float baseGravity = 5f;
     [SerializeField] float horizontal;
+    [SerializeField] float vertical;
+    [SerializeField] bool isTouchingLadder;
     #endregion
 
     #region Ground Check
@@ -32,7 +37,10 @@ public class Player_Controller : MonoBehaviour
         private set
         { 
             groundCheck = value;
-            anim.SetBool("isJumping", !value);
+            if (!isTouchingLadder)
+            {
+                anim.SetBool("isJumping", !value);
+            }
         }
     }
     #endregion
@@ -44,27 +52,38 @@ public class Player_Controller : MonoBehaviour
         capsCollider = GetComponent<CapsuleCollider2D>();
     }
 
-
+    #region Updates
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        //make player move
-        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
-        
-
-        //Methods
         GroundCheck();
         FlipSrpite();
-        //anim.SetBool("isJumping", !isGrounded);
-        anim.SetFloat("yVelocity", rb.velocity.y);
+        LadderCheck();
     }
 
+    private void FixedUpdate()
+    {
+        if (!isTouchingLadder)
+        {
+            rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+            rb.gravityScale = baseGravity;
+        }
+        else
+        {
+            rb.velocity = new Vector2(horizontal * 0.5f, vertical * climbSpeed);
+            rb.gravityScale = 0f;
+        }
+        anim.SetFloat("yVelocity", rb.velocity.normalized.y);
+    }
+    #endregion
+
+    #region Input Actions
     public void OnJump(InputAction.CallbackContext context)
     {
         
         if (isGrounded && context.performed)
         {
-            //anim.SetBool("isJumping", true);
+            anim.SetBool("isJumping", true);
+            //isGrounded = false;
             rb.velocity = new Vector2(rb.velocity.y, jumpPower);
         }
         else if (context.canceled && rb.velocity.y != 0)
@@ -72,11 +91,9 @@ public class Player_Controller : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
     }
-
     public void OnMove(InputAction.CallbackContext context)
-    {
-        horizontal = context.ReadValue<Vector2>().normalized.x;
-
+    { 
+        horizontal = context.ReadValue<Vector2>().x;
         //Running Animation
         if (horizontal != 0)
         {
@@ -87,18 +104,33 @@ public class Player_Controller : MonoBehaviour
             anim.SetBool("isMoving", false);
         }
     }
+    public void OnLadder(InputAction.CallbackContext context)
+    {
+        vertical = context.ReadValue<Vector2>().y;
+    }
+    #endregion
 
     void FlipSrpite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(horizontal) > Mathf.Epsilon;
         if (playerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(horizontal), 1f);
         }
     }
-
     void GroundCheck()
     {
         isGrounded = capsCollider.Cast(Vector2.down, groundCheckFilter, groundCheckHit, groundCheckDistance) > 0;
+    }
+    void LadderCheck()
+    {
+        if (capsCollider.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            isTouchingLadder = true;
+        }
+        else
+        {
+            isTouchingLadder = false;
+        }
     }
 }
